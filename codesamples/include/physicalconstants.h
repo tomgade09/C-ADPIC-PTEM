47fb815fa9bf8d2f7fb998d9084ac57c36cb121d
Tom Gade <tomgade09@gmail.com>
2017-08-15 14:55:42 -0500

    Removed code samples.

--- codesamples/include/physicalconstants.h
+++
@@ -1,9 +0,0 @@
-#ifndef PHYSICALCONSTANTS_H
-#define PHYSICALCONSTANTS_H
-
-const auto pi_c{ 3.141592653589793238463 };	  //unitless
-const auto c_c{ 299792458. };				  //[m/s]
-const auto mu0_c{ 4e-7 * PI_C };			  //[H/m] or [m kg / (s^2 A^2)]
-const auto eps0_c{ 1 / (c_c * c_c * mu0_c) }; //[F/m] or [A^2 s^4 / (m^3 kg)]
-
-#endif
--- codesamples/include/readsettings.h
+++
@@ -1,7 +0,0 @@
-#ifndef READSETTINGS_H
-#define READSETTINGS_H
-#include <vector>
-
-std::vector<double> readSettingsCSV(int argc, char *argv[]);
-
-#endif
